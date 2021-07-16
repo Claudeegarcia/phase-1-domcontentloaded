e8ad4210e22f031d489ff56a6f9169ab64e61e0d
Claudia <claudeegarcia@icloud.com>
2021-07-16 09:33:32 -0500

    commit tree 7a63135073fc3ff0e9002bd7aae12f7abbecad71

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+  document.addEventListener("DOMContentLoaded", function() {
+    let banana = document.getElementById('text')
+    banana.textContent = "This is really cool!"
+  });
+  
+
+
+
+  //class EventEmitter {
+  //constructor(id) {
+    //this.events = {};
+    //this.id = id;
+  //}
+
+  //addEventListener(eventType, listener) {
+    //if (!this.events[eventType]) {
+      //this.events[eventType] = [];
+    //}
+
+    //this.events[eventType].push(listener);
+  //}
+
+  //fire(eventType, eventProperties) {
+    //if (this.events[eventType]) {
+      //this.events[eventType].forEach(listener => listener(eventProperties));
+    //}
+  //}
+//}
